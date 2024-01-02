870cda010919d79bc7ae39e5d59d10fb8af3e1de
CoderEx24 <coderex24@protonmail.com>
2024-01-02 23:48:16 +0200

    started to link to database

--- login.py
+++ login.py
@@ -1,22 +1,32 @@
 from kivy.uix.screenmanager import Screen
 from kivy.lang import Builder
 from kivy.clock import Clock
+from os import environ
 from os import path
+import oracledb
 
 Builder.load_file(path.join(path.dirname(__file__), 'kv', 'login.kv'))
 
 class LoginScreen(Screen):
     
     def login(self):
-        error_msg = self.ids['error_widget']
-        error_msg.opacity = 1
-        error_msg.text = f'ERROR: Test'
-        error_msg.size_hint_y = 0.5
+        username = self.ids['username']
+        password = self.ids['password']
 
-        def _rehide_err(_):
-            error_msg.opacity = 0
-            error_msg.size_hint_y = 0
 
-        Clock.schedule_once(_rehide_err, 5)
+        try:
+            self.app.db_connection = oracledb.connect(user=username, password=password, \
+                    dsn=environ.get('ORACLE_URL'))
+        except Exception:
+            error_msg = self.ids['error_widget']
+            error_msg.opacity = 1
+            error_msg.text = f'ERROR: Test'
+            error_msg.size_hint_y = 0.5
+
+            def _rehide_err(_):
+                error_msg.opacity = 0
+                error_msg.size_hint_y = 0
+
+            Clock.schedule_once(_rehide_err, 5)
         
 
--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from kivy.uix.anchorlayout import AnchorLayout
+from kivy.properties import ObjectProperty
 from kivy.lang import Builder
 from kivy.app import App
 from os import path
+from master import *
 from login import *
+import oracledb
 
 Builder.load_file(path.join(path.dirname(__file__), 'kv', 'main.kv'))
 
@@ -10,6 +13,9 @@ class MainScreen(AnchorLayout):
     pass
 
 class MainApp(App):
+
+    db_connection = ObjectProperty(None) 
+
     def build(self):
         return MainScreen()
 
