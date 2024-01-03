7624dfcaba40cf4e9cff09b7a1a5bead50471276
CoderEx24 <coderex24@protonmail.com>
2024-01-03 18:31:30 +0200

    implemented basic master screen ui behaviour

--- master.py
+++ master.py
@@ -1,13 +1,13 @@
-from kivy.uix.recycleview.views import RecycleDataViewBehavior
 from kivy.properties import StringProperty
 from kivy.uix.screenmanager import Screen
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.widget import Widget
 from kivy.lang import Builder
 from os import path
 
 Builder.load_file(path.join(path.dirname(__file__), 'kv', 'master.kv'))
 
-class CourseEntry(RecycleDataViewBehavior, Widget):
+class CourseEntry(BoxLayout):
     title_text = StringProperty()
 
 class MasterScreen(Screen):
