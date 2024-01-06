212865016342ba2dd53eb56463035a0f3a1703d6
CoderEx24 <coderex24@protonmail.com>
2024-01-06 09:42:03 +0200

    minor refactor to login logic

--- login.py
+++ login.py
@@ -1,6 +1,7 @@
 from kivy.uix.screenmanager import Screen
 from kivy.lang import Builder
 from kivy.clock import Clock
+from kivy.app import App
 from os import environ
 from os import path
 import oracledb
@@ -14,9 +15,8 @@ class LoginScreen(Screen):
         username = self.ids['username']
         password = self.ids['password']
 
-
         try:
-            self.app.db_connection = oracledb.connect(user=username, password=password, dsn=environ.get('ORACLE_URL'))
+            App.get_running_app().db_connection = oracledb.connect(user=username, password=password, dsn=environ.get('ORACLE_URL'))
             instructor_id = self.authenticate(username, password)
         except Exception:
             error_msg = self.ids['error_widget']
@@ -32,11 +32,10 @@ class LoginScreen(Screen):
     
     def authenticate(self, username, password):
         try:
-            cursor = self.app.db_connection.cursor()
-            cursor.execute("SELECT InstructorID FROM Instructors WHERE Email = :email AND Password = :password", email=username, password=password)
-            result = cursor.fetchone()
-            cursor.close()
-            return result[0] if result else None
+            with App.get_running_app().db_connection.cursor() as cursor:
+                cursor.execute("SELECT InstructorID FROM Instructors WHERE Email = :email AND Password = :password", email=username, password=password)
+                result = cursor.fetchone()
+                return result[0] if result else None
         except cx_Oracle.DatabaseError as e:
             print("Database error during authentication:", e)
             return None
